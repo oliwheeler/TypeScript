eaad87e7c3402af5efbed9e26bbd41c658145af8
Oliver Wheeler <oliver.kuruma@gmail.com>
2021-01-18 03:27:41 +0100

    Add resolutionPlatforms option/env for better React Native support (with .d.ts fix)

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -5989,6 +5989,7 @@ namespace ts {
         incremental?: boolean;
         tsBuildInfoFile?: string;
         removeComments?: boolean;
+        resolutionPlatforms?: string[]; // Use react-native lookup logic for these platforms
         rootDir?: string;
         rootDirs?: string[];
         skipLibCheck?: boolean;
